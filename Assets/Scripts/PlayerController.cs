d36a00903945e2d215f14927b77b4a100e7a00bf
Pavlo Shchelkun <pavel.shchelkun@gmail.com>
2017-01-17 20:00:38 +0200

    Moving the Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 10f;
+
+    private Rigidbody _rb;
+    
+    private void Start ()
+    {
+        _rb = GetComponent<Rigidbody>();
+	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        _rb.AddForce(movement * Speed);
+    }
+}

f46642baca4553a2bff5bd364eb9f5da4c597aaf
Pavlo Shchelkun <pavel.shchelkun@gmail.com>
2017-01-19 17:05:34 +0200

    Displaying the Score and Text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float Speed = 10f;
+    public Text CountText;
+    public Text WinText;
 
     private Rigidbody _rb;
+    private int _count;
     
     private void Start ()
     {
         _rb = GetComponent<Rigidbody>();
-	}
+        _count = 0;
+        SetCountText();
+        WinText.text = "";
+    }
 
     private void FixedUpdate()
     {
@@ -26,6 +33,18 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            _count = _count + 1;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        CountText.text = "Count: " + _count.ToString();
+
+        if (_count >= 12)
+        {
+            WinText.text = "You win!";
         }
     }
 }
